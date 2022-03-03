298296d029b9570f825a1e657fa40f66f8ace3be
Marco Mariani <marco@crowdsec.net>
2022-03-03 12:43:28 +0100

    extract functions getUID/getGID

--- pkg/csplugin/utils.go
+++ pkg/csplugin/utils.go
@@ -62,33 +62,49 @@ func getPluginTypeAndSubtypeFromPath(path string) (string, string, error) {
 	return strings.Join(parts[:len(parts)-1], "-"), parts[len(parts)-1], nil
 }
 
-func getProcessAttr(username string, groupname string) (*syscall.SysProcAttr, error) {
+func getUID(username string) (uint32, error) {
 	u, err := user.Lookup(username)
 	if err != nil {
-		return nil, err
-	}
-	g, err := user.LookupGroup(groupname)
-	if err != nil {
-		return nil, err
+		return 0, err
 	}
 	uid, err := strconv.ParseInt(u.Uid, 10, 32)
 	if err != nil {
-		return nil, err
+		return 0, err
 	}
 	if uid < 0 || uid > math.MaxInt32 {
-		return nil, fmt.Errorf("out of bound uid")
+		return 0, fmt.Errorf("out of bound uid")
+	}
+	return uint32(uid), nil
+}
+
+func getGID(groupname string) (uint32, error) {
+	g, err := user.LookupGroup(groupname)
+	if err != nil {
+		return 0, err
 	}
 	gid, err := strconv.ParseInt(g.Gid, 10, 32)
 	if err != nil {
-		return nil, err
+		return 0, err
 	}
 	if gid < 0 || gid > math.MaxInt32 {
-		return nil, fmt.Errorf("out of bound gid")
+		return 0, fmt.Errorf("out of bound gid")
+	}
+	return uint32(gid), nil
+}
+
+func getProcessAttr(username string, groupname string) (*syscall.SysProcAttr, error) {
+	uid, err := getUID(username)
+	if err != nil {
+		return nil, err
+	}
+	gid, err := getGID(groupname)
+	if err != nil {
+		return nil, err
 	}
 	return &syscall.SysProcAttr{
 		Credential: &syscall.Credential{
-			Uid: uint32(uid),
-			Gid: uint32(gid),
+			Uid: uid,
+			Gid: gid,
 		},
 	}, nil
 }
